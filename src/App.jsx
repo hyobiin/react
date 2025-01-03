cf58b9017fa3c354a659c35b4af4eb7bc0629884
hyobin15 <hyobin15@finenuts.co.kr>
2025-01-03 16:13:34 +0900

    sort() 정렬

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,23 @@ function App() {
   // 하나로 합치기
   let [state, setState] = useState({ 좋아요: 0, 좋아요2: 0 });
 
+  let [나이, 나이변경] = useState([
+      ['민수', 20],
+      ['영희', 32],
+      ['순신', 5],
+      ['맹구', 1],
+  ]);
+
   return (
     <>
       <div className="test">
+        <button onClick={()=>
+          {
+            let copy = [...구이름];
+            copy.sort();
+            구이름변경(copy);
+          }
+        }>가나다순</button>
         <span>{ 구이름[0] }
           <span onClick={() => {
             let newName = [...구이름];
@@ -32,6 +46,29 @@ function App() {
             setState({...state, 좋아요2: 0})
           }}>초기화</span>
         </span>
+
+        <span>{ 구이름[2] }</span>
+
+        <button onClick={() => {
+          let name = [...나이];
+          name.sort((a, b) => a[0].localeCompare(b[0])); // 이름 기준으로 가나다순 정렬
+          // name.sort((a, b) => b[0].localeCompare(a[0])); // 이름 기준으로 가나다순 내림차순 정렬
+          나이변경(name);
+        }}>이름 정렬</button>
+
+        <button onClick={() => {
+          let age = [...나이];
+          age.sort((a, b) => a[1] - b[1]); // 나이 기준으로 오름차순 정렬
+          // age.sort((a, b) => b[1] - a[1]); // 나이 기준으로 내림차순 정렬
+          나이변경(age);
+        }}>나이 정렬</button>
+
+        <ul>
+          <li>{나이[0][0]}: {나이[0][1]}</li>
+          <li>{나이[1][0]}: {나이[1][1]}</li>
+          <li>{나이[2][0]}: {나이[2][1]}</li>
+          <li>{나이[3][0]}: {나이[3][1]}</li>
+        </ul>
       </div>
     </>
   )
